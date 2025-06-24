d625bc982f5dfdfa73ada52fbcdb3a4e98e874ed
ogmini <112595633+ogmini@users.noreply.github.com>
2025-06-24 11:06:16 -0400

    WindowStateEditor

--- NotepadStateLibrary/NotepadStateLibrary/NPWindowState.cs
+++ NotepadStateLibrary/NotepadStateLibrary/NPWindowState.cs
@@ -94,93 +94,8 @@ namespace NotepadStateLibrary
         /// <param name="NewActiveTab">Integer of the active tab. 0 based index.</param>
         public void ChangeActiveTab(int NewActiveTab)
         {
-            using (MemoryStream stream = new MemoryStream(bytes))
-            {
-                using (MemoryStream outStream = new MemoryStream())
-                {
-                    using (BinaryReader reader = new BinaryReader(stream))
-                    {
-                        using (BinaryWriter writer = new BinaryWriter(outStream))
-                        {
-                            var hdr = reader.ReadBytes(2);
-                            writer.Write(hdr);
-                            string hdrType = Encoding.ASCII.GetString(hdr);
-                            if (hdrType == "NP")
-                            {
-                                CRC32Check c = new CRC32Check();
-
-                                reader.ReadLEB128Unsigned();
-                                writer.Write(SequenceNumber.WriteLEB128Unsigned());
-                                c.AddBytes(SequenceNumber);
-
-                                reader.ReadLEB128Unsigned();
-                                writer.Write(BytesToCRC); 
-                                c.AddBytes(BytesToCRC);
-
-
-                                var delim = reader.ReadBytes(1);
-                                writer.Write(delim);
-                                c.AddBytes(delim);
-
-                                reader.ReadLEB128Unsigned();
-                                writer.Write(NumberTabs.WriteLEB128Unsigned());
-                                c.AddBytes(NumberTabs);
-
-                                for (int x = 0; x < (int)NumberTabs; x++)
-                                {
-                                    var chunk = reader.ReadBytes(16);
-                                    writer.Write(chunk);
-                                    c.AddBytes(chunk);
-                                }
-
-                                reader.ReadLEB128Unsigned(); //Active Tab
-                                ActiveTab = (ulong)NewActiveTab;
-                                writer.Write(ActiveTab.WriteLEB128Unsigned());
-                                c.AddBytes(ActiveTab);
-
-                                var tlc1 = reader.ReadUInt32();
-                                writer.Write(tlc1);
-                                c.AddBytes(tlc1);
-                                var tlc2 = reader.ReadUInt32();
-                                writer.Write(tlc2);
-                                c.AddBytes(tlc2);
-
-                                TopLeftCoords = new WindowXY((int)tlc1, (int)tlc2);
-
-
-                                var brc3 = reader.ReadUInt32();
-                                writer.Write(brc3);
-                                c.AddBytes(brc3);
-                                var brc4 = reader.ReadUInt32();
-                                writer.Write(brc4);
-                                c.AddBytes(brc4);
-
-                                BottomRightCoords = new WindowXY((int)brc3, (int)brc4);
-
-
-                                var wsc5 = reader.ReadUInt32();
-                                writer.Write(wsc5);
-                                c.AddBytes(wsc5);
-                                var wsc6 = reader.ReadUInt32();
-                                writer.Write(wsc6);
-                                c.AddBytes(wsc6);
-
-                                WindowSize = new WindowXY((int)wsc5, (int)wsc6);
-
-
-                                var delim2 = reader.ReadBytes(1);
-                                writer.Write(delim2);
-                                c.AddBytes(delim2);
-
-                                reader.ReadBytes(4);
-                                CRC32Stored = c.CRC32;
-                                writer.Write(CRC32Stored);
-                                CRC32Calculated = c.CRC32;
-                            }
-                        }
-                    }
-                }
-            }
+            this.ActiveTab = (ulong)NewActiveTab;
+            Save();
         }
 
         /// <summary>
@@ -191,6 +106,18 @@ namespace NotepadStateLibrary
         {
             NumberTabs = (ulong)newTabs.Count;
             Tabs = newTabs;
+
+            Save();
+        }
+
+        public void WriteTabList(List<Guid> newTabs)
+        {
+            NumberTabs = (ulong) newTabs.Count;
+            Tabs = new List<byte[]>();
+            foreach(Guid g in newTabs)
+            {
+                Tabs.Add(g.ToByteArray());
+            }
 
             Save();
         }
---
+++ NotepadStateLibrary/WindowStateEditor/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NotepadStateLibrary;
+using CommandLine;
+using System.Diagnostics;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Text;
+using System.ComponentModel;
+
+
+Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(options =>
+            {
+                string windowStateLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Packages\Microsoft.WindowsNotepad_8wekyb3d8bbwe\LocalState\WindowState");
+                List<Guid> guidList = new List<Guid>();
+
+
+                if (!string.IsNullOrWhiteSpace(options.windowStateLocation))
+                {
+                    windowStateLocation = options.windowStateLocation;
+                }
+
+                foreach (var stringGuid in options.guidTabs ?? new List<string>())
+                {
+                    if (Guid.TryParse(stringGuid, out Guid guid))
+                    {
+                        guidList.Add(guid);
+                        Console.WriteLine("Add GUID: {0}", stringGuid);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid GUID: {0}", stringGuid);
+                    }
+                }
+
+                Console.WriteLine("********** Starting **********");
+
+                foreach (var path in Directory.EnumerateFiles(windowStateLocation, "*.bin"))
+                {
+                    byte[] o = new byte[0];
+
+                    using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    {
+                        byte[] data = new byte[fileStream.Length];
+                        fileStream.Read(data);
+
+                        if (data.Length > 0)
+                        {
+                            Console.WriteLine("Processing WindowState - {0}", Path.GetFileName(path));
+                            NPWindowState np = new NPWindowState(data, Path.GetFileName(path));
+
+                            if (guidList.Count > 0)
+                            {
+                                np.WriteTabList(guidList);
+
+                                np.ChangeActiveTab(options.activeTab);
+
+                            }
+
+                            
+
+                            o = np.bytes;
+                        }
+                    }
+                    File.WriteAllBytes(path, o);
+                }
+
+                Console.WriteLine("********** Finished **********");
+            })
+            .WithNotParsed(errors =>
+            {
+                foreach (var error in errors)
+                {
+                    if (error is HelpRequestedError || error is VersionRequestedError)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {error}");
+                    }
+                }
+            });
+
+
+public class Options
+{
+    [Option('w', "windowstatelocation", Required = false, HelpText = "Window State Folder Location. Default value is the system location.")]
+    public string windowStateLocation { get; set; }
+
+    [Option('t', "tabs", Required = true, HelpText = "Space seperated GUIDs to write to Window State File.")]
+    public IEnumerable<string> guidTabs { get; set; }
+
+    [Option('a', "activeTab", Required = false, HelpText = "0 based index of the active tab. Default value is 0", Default = 0)]
+    public int activeTab { get; set; }
+}
+
+
